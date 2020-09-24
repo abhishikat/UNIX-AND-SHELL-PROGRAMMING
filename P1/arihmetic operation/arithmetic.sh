1fc828e71a5db999973b0d9e67a288184aae9c53
abhishikat <53806537+abhishikat@users.noreply.github.com>
2020-09-24 15:26:01 +0530

    Add files via upload

---
+++ P1/arihmetic operation/arithmetic.sh
@@ -0,0 +1,19 @@
+a=10
+b=20
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % a : $val"
+
+
